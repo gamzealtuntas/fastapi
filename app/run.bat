54a0b8006ed0aa63d05e9cb652c843a63f14bb24
gamzealtuntas <gamzealtuntas5885@gmail.com>
2025-08-28 12:58:06 +0300

    fastapi

--- app/run.bat
+++ app/run.bat
@@ -1,5 +1,5 @@
 @echo off
-echo 🚀 FastAPI başlatılıyor...
-start "" http://127.0.0.1:8000/docs
-uvicorn main:app --reload
+echo  FastAPI başlatılıyor...
+start http://127.0.0.1:8000/docs
+uvicorn app.main:app --reload
 pause
